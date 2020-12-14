221afc6b65cd84f3e1533803b48123f73dd2c7c1
K. Stavrakakis <kostantinosst23@gmail.com>
2020-12-14 17:11:51 +0200

    Delete EmployeeService.java

--- mavenspringtest/src/main/java/com/mavespringtest/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.mavespringtest.service;
-
-import java.util.List;
-
-import com.mavespringtest.model.Employee;
-
-public interface EmployeeService {
-	List<Employee> getAllEmployees();
-}
